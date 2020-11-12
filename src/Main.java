2aa75473ff12a085b19c420bb902eaea49dad1bf
asuman75 <74166822+asuman75@users.noreply.github.com>
2020-11-12 10:47:06 -0500

    b21 dedicated

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,13 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello Git Last Day");
+      // B21 is exeted
+      // b21 is dedicated
+      //B21 is succesful testers
+
+
+
+
+
     }
 }
